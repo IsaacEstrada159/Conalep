3085ec2c6e2f6d105df9c1c1183a8c086094a127
IsaacEstrada159 <70659682+IsaacEstrada159@users.noreply.github.com>
2022-11-29 16:41:17 -0600

    Update and rename rgsrgrg.py to dos dados.py

--- codigos/codigo nuevo/dos dados.py
+++ codigos/codigo nuevo/dos dados.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import random
-import scipy.stats as stats
-import seaborn as sns
-import matplotlib.pyplot as plt
 lista = [1,2,3,4,5,6]
-tamaño = 100000
+tamaño = 300000
 lista2 = []
 lista3 = []
 for i in range(tamaño):
@@ -14,7 +11,7 @@ df2 = pd.DataFrame(df)
 for j in range(1,7):
   df2 = (j/6)/df
   df2 = df2.mean()
-  lista3.append(df2)
+  lista3.append(float(df2))
 
 data = {'probabilidades': ['dos','tres','cuatro','cinco','seis', 'siete'],'datos': lista3}
 df3 = pd.DataFrame(data)
